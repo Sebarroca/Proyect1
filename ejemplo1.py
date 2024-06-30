462c3baca1bd80d86aa4ed9dc2beae414a1616f7
Sebastian Barroca <barroca.s@hotmail.com>
2024-06-30 19:49:41 -0300

    regrese desde el pasado

--- ejemplo1.py
+++ ejemplo1.py
@@ -2,6 +2,9 @@ def mensaje():
     print("estoy trabajando con git")
 mensaje()
 
+def mensaje2():
+    print("Volvi al pasado")
+    
 def sumar(n1,n2):
     print("el resultado de la suma entre n1 + n2 es de:",n1 + n2)
 sumar(int(input("Ingrese un numero:")),int(input("Ingrese otro numero:")))
