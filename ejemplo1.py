bea6bb82d2072ac187fb1d4aa7206e31922ddb0d
Sebastian Barroca <barroca.s@hotmail.com>
2024-06-30 19:18:52 -0300

    tercera version, con multiplicar y dividir

--- ejemplo1.py
+++ ejemplo1.py
@@ -5,3 +5,7 @@ mensaje()
 def sumar(n1,n2):
     print("el resultado de la suma entre n1 + n2 es de:",n1 + n2)
 sumar(int(input("Ingrese un numero:")),int(input("Ingrese otro numero:")))
+
+def multiplicar(n1,n2):
+    print("el resultado de la multiplicación entre n1 * n2 es de:",n1 * n2)
+multiplicar(int(input("Ingrese un numero:")),int(input("Ingrese otro numero:")))
--- ejemplo2.py
+++ ejemplo2.py
@@ -1,3 +1,7 @@
 def restar(n1,n2):
     print("el resultado de la resta entre n1 - n2 es de:",n1 - n2)
 restar(int(input("Ingrese un numero:")),int(input("Ingrese otro numero:")))
+
+def dividir(n1,n2):
+    print("el resultado de la division entre n1 / n2 es de:",n1 / n2)
+dividir(int(input("Ingrese un numero:")),int(input("Ingrese otro numero:")))
